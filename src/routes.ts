522dbc66324c271d924724f259f6ab53e2079d03
FelipeCoutinho <philipecout@gmail.com>
2020-09-10 14:43:16 -0300

    remoção do padrão antigo de controlleres

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 import {Router, } from 'express'
-import {getTask,getTasks,saveTask,updateTask,destroi,finisheTask} from './controllers/taskController'
 import Task from './controllers/task';
 
 
@@ -11,15 +10,7 @@ route.get('/find/:id', task.show)
 route.post('/newTask', task.store)
 route.delete('/delete/:id', task.desctroy)
 route.put('/updateTask/:id', task.update)
-route.put('/finishedTask/:id', task.finishedTask)
-
-
-route.get('/tasks', getTasks)
-route.get('/tasks/:id', getTask)
-route.post('/cad', saveTask)
-route.put('/update/:id',updateTask)
-route.delete('/del/:id',destroi)
-route.patch('/save/:id',finisheTask) // indicapara alterar só um registro no banco
+route.patch('/finishedTask/:id', task.finishedTask)
 
 export default route
 
